54a62a123c172724bc4b41832fc824bd24608061
GyryProblem <49600081+GyryProblem@users.noreply.github.com>
2019-05-17 23:26:36 +0300

    Initial commit

---
+++ Calculation.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCP1
+{
+    static class Calculation
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new CalculationForm());
+        }
+    }
+    
+    public class Calculations
+    {
+        public static int[] rg_a = new int[8];
+        public static int[] rg_b = new int[8];
+        public static bool count = false;
+        public static int[] rg_c = new int[8];
+        public static bool overflow = false;
+
+        public static void Inverse(int[] rg) //Перевод числа в дополнительный код
+        {
+            for (int i = 1; i < rg.Length; i++)
+            {
+                rg[i] = (rg[i] + 1) % 2;
+            }
+            bool increase = true;
+            int col = 7;
+            int more = 1;
+            while (increase)
+            {
+                rg[col] = rg[col] + more;
+                if (rg[col] > 1)
+                {
+                    rg[col] = rg[col] - 2;
+                    col--;
+                }
+                else
+                {
+                    increase = false;
+                }
+            }
+        }
+
+        public static void Copy(string copy_str) //Запись в переменные
+        {
+            if (!count)
+            {
+                for (int i = 0; i < copy_str.Length; i++)
+                {
+                    rg_a[i] = Convert.ToInt16(copy_str[i]) - 48;
+                }
+                count = true;
+            }
+            else
+            {
+                for (int i = 0; i < copy_str.Length; i++)
+                {
+                    rg_b[i] = Convert.ToInt16(copy_str[i]) - 48;
+                }
+                count = false;
+            }
+        }
+        public static void Operation() //Сложение двух 8-ми разрядных чисел для 8-ми разрядного сумматора
+        {
+            rg_c = new int[8] { 0, 0, 0, 0, 0, 0, 0, 0 };
+            overflow = false;
+            int ct = 7;
+            int tg_p = 0;
+            int step = 1;
+            do
+            {
+                rg_c[ct] = rg_a[ct] + rg_b[ct] + tg_p;
+                if (rg_c[ct] > 1)
+                {
+                    if (ct == 0)
+                    {
+                        if (tg_p == 0)
+                        {
+                            overflow = true;
+                        }
+                    }
+                    tg_p = 1;
+                    rg_c[ct] = rg_c[ct] - 2;
+                }
+                else
+                {
+                    if (ct == 0)
+                    {
+                        if (tg_p == 1)
+                        {
+                            overflow = true;
+                        }
+                    }
+                    tg_p = 0;
+                }
+                ct--;
+                step++;
+            } while (ct >= 0);
+            if ((rg_c[0] == 1)&&(!overflow))
+            {
+                Inverse(rg_c);
+            }
+        }
+    }
+}
---
+++ CalculationForm.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TCP1
+{
+    public partial class CalculationForm : Form
+    {
+        public CalculationForm()
+        {
+            InitializeComponent();
+        }
+
+        private bool CheckKey(char number)  //Возможен ввод 1 или 0
+        {
+            if ((number <= 47 || number >= 50) && number != 8)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void Increase() //Заполнение до 8 разрядов 
+        {
+            while (RgABox.Text.Length < 8)
+            {
+                RgABox.Text = RgABox.Text + '0';
+            }
+            while (RgBBox.Text.Length < 8)
+            {
+                RgBBox.Text = RgBBox.Text + '0';
+            }
+        }
+
+        private void RgABox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            e.Handled = CheckKey(number);
+        }
+
+        private void RgBBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            char number = e.KeyChar;
+            e.Handled = CheckKey(number);
+        }
+
+        private void OperationPlus_Click(object sender, EventArgs e)
+        {
+            Increase();
+            InfoBox.Clear();
+            InfoBox.Text = "RgA: ";
+            RgCBox.Clear();
+            for (int i = 0; i < RgABox.Text.Length; i++)
+            {
+                InfoBox.Text = InfoBox.Text + RgABox.Text[i];
+                if (i == 0)
+                {
+                    InfoBox.Text = InfoBox.Text + ',';
+                }
+            }
+            InfoBox.Text = InfoBox.Text + "\r\n";
+            InfoBox.Text = InfoBox.Text + "RgB: ";
+            for (int i = 0; i < RgBBox.Text.Length; i++)
+            {
+                InfoBox.Text = InfoBox.Text + RgBBox.Text[i];
+                if (i == 0)
+                {
+                    InfoBox.Text = InfoBox.Text + ',';
+                }
+            }
+            InfoBox.Text = InfoBox.Text + "\r\n";
+            InfoBox.Text = InfoBox.Text + "Operation +" + "\r\n" + "\r\n";
+            Calculations.Copy(RgABox.Text);
+            Calculations.Copy(RgBBox.Text);
+            int[] check = new int[8] { 1, 0, 0, 0, 0, 0, 0, 0};
+            if ((Calculations.rg_a[0] == 1)&&(!Equals(Calculations.rg_a, check)))
+            {
+                Calculations.Inverse(Calculations.rg_a);
+            }
+            if ((Calculations.rg_b[0] == 1)&&(!Equals(Calculations.rg_b, check)))
+            {
+                Calculations.Inverse(Calculations.rg_b);
+            }
+            Calculations.Operation();
+            if (!Calculations.overflow)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    RgCBox.Text = RgCBox.Text + Convert.ToString(Calculations.rg_c[i]);
+                    if (i == 0)
+                    {
+                        RgCBox.Text = RgCBox.Text + ',';
+                    }
+                }
+            }
+            else
+            {
+                InfoBox.Text = InfoBox.Text + "Возникло переполнение!";
+            }
+        }
+
+        private void OperatonMinus_Click(object sender, EventArgs e)
+        {
+            Increase();
+            InfoBox.Clear();
+            InfoBox.Text = "RgA: ";
+            RgCBox.Clear();
+            for (int i = 0; i < RgABox.Text.Length; i++)
+            {
+                InfoBox.Text = InfoBox.Text + RgABox.Text[i]; 
+                if (i == 0)
+                {
+                    InfoBox.Text = InfoBox.Text + ',';
+                }
+            }
+            InfoBox.Text = InfoBox.Text + "\r\n";
+            InfoBox.Text = InfoBox.Text + "RgB: ";
+            for (int i = 0; i < RgBBox.Text.Length; i++)
+            {
+                InfoBox.Text = InfoBox.Text + RgBBox.Text[i];
+                if (i == 0)
+                {
+                    InfoBox.Text = InfoBox.Text + ',';
+                }
+            }
+            InfoBox.Text = InfoBox.Text + "\r\n";
+            InfoBox.Text = InfoBox.Text + "Operation -" + "\r\n" + "\r\n";
+            Calculations.Copy(RgABox.Text);
+            Calculations.Copy(RgBBox.Text);
+            int[] check = new int[8] { 1, 0, 0, 0, 0, 0, 0, 0 };
+            if ((Calculations.rg_a[0] == 1) && (!Equals(Calculations.rg_a, check)))
+            {
+                Calculations.Inverse(Calculations.rg_a);
+            }
+            if (Calculations.rg_b[0] == 0)
+            {
+                Calculations.rg_b[0] = 1;
+                if (!Equals(Calculations.rg_b, check))
+                {
+                    Calculations.Inverse(Calculations.rg_b);
+                }
+            }
+            else
+            {
+                Calculations.rg_b[0] = 0;
+            }
+            Calculations.Operation();
+            if (!Calculations.overflow)
+            {
+                for (int i = 0; i < 8; i++)
+                {
+                    RgCBox.Text = RgCBox.Text + Convert.ToString(Calculations.rg_c[i]);
+                    if (i == 0)
+                    {
+                        RgCBox.Text = RgCBox.Text + ',';
+                    }
+                }
+            }
+            else
+            {
+                InfoBox.Text = InfoBox.Text + "Возникло переполнение!";
+            }
+        }
+    }
+}
